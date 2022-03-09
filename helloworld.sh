99254a7c83e51eb5ae4339a0647cf21624bca1c5
chapakook <chapakook96@gmail.com>
2022-03-09 21:28:19 +0900

    shell script hello world

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Colors
+RED="\033[31m"
+BLUE="\033[34m"
+B_BLUE="\033[34;1m"
+B_GREEN="\033[31;1m"
+BOLD="\033[01m"
+WHITE="\033[0m"
+
+echo $RED
+echo $BOLD
+echo "hello world"
+echo $WHITE
